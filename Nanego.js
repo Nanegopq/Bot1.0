9309663d6b159b291def9cc9a12bc942ec2f3abc
Nanego <87090157+Nanegopq@users.noreply.github.com>
2021-11-17 20:05:53 -0400

    Create Nanego.js

---
+++ Nanego.js
@@ -0,0 +1,68 @@
+
+
+const { WAConnection } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function iniciar () { 
+        const client = new WAConnection()
+
+        client.logger.level = 'warn'
+
+        client.on('qr', () => {
+        })
+
+        fs.existsSync('./nanego.json') && client.loadAuthInfo('./nanego.json')
+
+        client.on('connecting', () => {
+        console.log('Conectando')
+        })
+
+        client.on('open', () => {
+        console.log('Conectado exitosamente :D')
+        })
+        await client.connect({timeoutMs: 120*1000})
+        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+        }
+
+iniciar ()
+.catch (err => console.log("unexpected error: " + err))
+
+
+
+        
+
+        
+        
+
+        
+
+        
+        
+        
+
+        
+        
+
+
+
+
+
+
+
+
+   
+        
+
+
+        
+
+         
+
+         
+
+
+
+        
+
+        
+   
